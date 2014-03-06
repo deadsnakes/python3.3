12198594995b266c6e90d0fe32cad6c7a7f0c568
Felix Krull <f_krull@gmx.de>
2014-03-06 22:32:57 +0100

    Made _sysconfigdata a bit smarter.
    Instead of guessing whether the current build is a debug build based on the
    presence of sys.gettotalrefcount and loading a _sysconfigdata module with a
    hardcoded name based on that, it now loads _sysconfigdata_${sys.abiflags)
    dynamically.

--- debian/_sysconfigdata.py
+++ debian/_sysconfigdata.py
@@ -1,6 +1,6 @@
+import importlib
 import sys
 
-if hasattr(sys, "gettotalrefcount"):
-    from _sysconfigdata_d import *
-else:
-    from _sysconfigdata_nd import *
+_sysconfigdata = importlib.import_module("_sysconfigdata_%s" % sys.abiflags)
+globals().update({k: v for k, v in _sysconfigdata.__dict__.items() if not k.startswith("_")})
+del importlib, sys, _sysconfigdata
